b0ee0493b81e02c29cf52b98203eec01c4e4c63d
slembcke <slembcke@gmail.com>
2008-11-16 11:34:31 +0000

    renaming rb_cpJoint.c to rb_cpConstraints.c, adding static lib build script for ruby binding

---
+++ ruby/make_static.sh
@@ -0,0 +1,7 @@
+# Builds Chipmunk as a static library
+# This will have to do until I can figure out how to properly use the Ruby build system.
+
+rm -f *.o
+find ../src -name "*.c" -print0 | xargs -0 gcc -c -std=gnu99 -ffast-math -O3 -I../src -I../src/constraint -arch ppc -arch i386
+ar rcs libChipmunk.a *.o
+rm -f *.o
