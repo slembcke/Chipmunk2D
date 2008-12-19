c2eb09752541bc75dafd3a80451764c78af197b6
slembcke <slembcke@gmail.com>
2008-12-19 07:51:43 +0000

    Rearranging ruby extension code

--- ruby/extconf.rb
+++
@@ -1,5 +0,0 @@
-require 'mkmf'
-
-$CFLAGS += ' -std=gnu99 -ffast-math -I../src -I../src/constraints -Wall -Werror'
-$LDFLAGS += ' ../libchipmunk.a'
-create_makefile('chipmunk')
--- ruby/make_static.sh
+++
@@ -1,7 +0,0 @@
-# Builds Chipmunk as a static library
-# This will have to do until I can figure out how to properly use the Ruby build system.
-
-rm -f *.o
-find ../src -name "*.c" -print0 | xargs -0 gcc -c -std=gnu99 -ffast-math -O3 -I../src -I../src/constraint -arch ppc -arch i386
-ar rcs libchipmunk.a *.o
-rm -f *.o
