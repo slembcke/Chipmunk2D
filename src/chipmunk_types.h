ba2bec7236f4aeb5ffc6bb6819659195a48d5eee
slembcke <slembcke@gmail.com>
2009-06-02 00:14:15 +0000

    Forgot to actually add the new header. (grumbles)

---
+++ src/chipmunk_types.h
@@ -0,0 +1,25 @@
+typedef double cpFloat;
+#define cpfsqrt sqrt
+#define cpfsin sin
+#define cpfcos cos
+#define cpfatan2 atan2
+#define cpfmod fmod
+#define cpfexp exp
+#define cpfpow pow
+#define cpffloor floor
+#define cpfceil ceil
+	
+//typedef float cpFloat;
+//#define cpfsqrt sqrtf
+//#define cpfsin sinf
+//#define cpfcos cosf
+//#define cpfatan2 atan2f
+//#define cpfmod fmodf
+//#define cpfexp expf
+//#define cpfpow powf
+//#define cpffloor floorf
+//#define cpfceil ceilf
+
+typedef unsigned int cpCollisionType;
+typedef unsigned int cpLayers;
+typedef unsigned int cpGroup;
