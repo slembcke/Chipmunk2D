6f30e6574074f9adc7805c26013f8a77a7bd2fb6
slembcke <slembcke@gmail.com>
2010-07-27 13:43:38 +0000

    updating the iphone build script to use OS 4

--- macosx/iphonestatic.sh
+++ macosx/iphonestatic.sh
@@ -1,3 +1,3 @@
-xcodebuild -project Chipmunk.xcodeproj -sdk iphoneos3.0 -configuration Release -target ChipmunkStatic-iPhone
-xcodebuild -project Chipmunk.xcodeproj -sdk iphonesimulator3.0 -configuration Debug -target ChipmunkStatic-iPhone
+xcodebuild -project Chipmunk.xcodeproj -sdk iphoneos4.0 -configuration Release -target ChipmunkStatic-iPhone
+xcodebuild -project Chipmunk.xcodeproj -sdk iphonesimulator4.0 -configuration Debug -target ChipmunkStatic-iPhone
 lipo build/Debug-iphonesimulator/libChipmunk-iPhone.a build/Release-iphoneos/libChipmunk-iPhone.a -create -output libChipmunk-iPhone.a
