483aba64ae91b8b383e614ddd602d8349cd9b7d6
slembcke <slembcke@gmail.com>
2010-08-10 00:55:35 +0000

    bumping the version number

--- src/chipmunk.c
+++ src/chipmunk.c
@@ -43,7 +43,7 @@ cpMessage(const char *message, const char *condition, const char *file, int line
 }
 
 
-const char *cpVersionString = "5.x.x";
+const char *cpVersionString = "5.3.x";
 
 void
 cpInitChipmunk(void)
