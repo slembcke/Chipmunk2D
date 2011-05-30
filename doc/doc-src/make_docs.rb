5eaa718e9d03197ac741bea30b1f6e95735bb858
slembcke <slembcke@gmail.com>
2011-05-30 19:30:48 +0000

    working on documentation

--- doc/doc-src/make_docs.rb
+++ doc/doc-src/make_docs.rb
@@ -1,5 +1,53 @@
 require 'rubygems'
 require 'redcloth'
+require 'erb'
+
+class Node
+	attr_reader :anchor
+	attr_reader :children
+	
+	def initialize(title, anchor, parent)
+		@title = title
+		@anchor = (parent.anchor ? "#{parent.anchor}-#{anchor}" : anchor)
+		
+		@children = []
+		parent.children << self
+	end
+	
+	def outline(level)
+		stars = "*"*level
+		children = @children.map{|child| child.outline(level + 1)}
+		(["#{stars} \"#{@title}\":##{anchor}"] + children).join("\n")
+	end
+	
+	Root = Struct.new(:anchor, :children).new(nil, [])
+	def Root.outline
+		self.children.map{|child| child.outline(1)}.join("\n")
+	end
+end
+
+def pop_open_div(name)
+	return <<-EOF
+		<a class="HideShow" href="javascript:;" onmousedown="toggleDiv('Pop Open #{name}');">Hide/Show #{name}</a>
+		<div class="PopOpen" id="Pop Open #{name}" style="display:none">
+	EOF
+end
+
+$node_path = [Node::Root]
+
+def h(level, title, anchor)
+	$node_path = $node_path[0, level]
+	parent = $node_path.last
+	
+	node = Node.new(title, anchor, parent)
+	$node_path << node
+	
+	"<a name=\"#{node.anchor}\" /><h#{level}><a href=\"##{node.anchor}\">#{title}</a></h#{level}>"
+end
+
+textile = ERB.new(File.read('chipmunk-docs.textile')).result(binding)
+textile_with_outline = ERB.new(textile).result(binding)
+redcloth_out = RedCloth.new(textile_with_outline).to_html
 
 html = <<HTML
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -10,7 +58,17 @@ html = <<HTML
 			<link rel="stylesheet" type="text/css" href="stylesheet.css" />
 		</head>
 		<body>
-			#{RedCloth.new(File.read('chipmunk-docs.textile')).to_html}
+			<script language="javascript">
+				function toggleDiv(divid){
+					if(document.getElementById(divid).style.display == 'none'){
+						document.getElementById(divid).style.display = 'block';
+					}else{
+						document.getElementById(divid).style.display = 'none';
+					}
+				}
+			</script>
+			
+			#{redcloth_out}
 		</body>
 	</html>
 HTML
