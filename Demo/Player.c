85ff1f6e174043bb196fb8ae9da7af158c7ab8f9
slembcke <slembcke@gmail.com>
2011-08-16 03:43:46 -0500

    Player controls

--- Demo/Player.c
+++ Demo/Player.c
@@ -26,71 +26,72 @@
 #include "chipmunk_private.h"
 #include "ChipmunkDemo.h"
 
+#define PLAYER_VELOCITY 600.0
+#define PLAYER_GROUND_ACCEL_TIME 0.1
+#define PLAYER_GROUND_ACCEL (PLAYER_VELOCITY/PLAYER_GROUND_ACCEL_TIME)
+#define PLAYER_AIR_ACCEL_TIME 0.2
+#define PLAYER_AIR_ACCEL (PLAYER_VELOCITY/PLAYER_AIR_ACCEL_TIME)
+#define JUMP_VELOCITY 1000.0
+#define GRAVITY 4000.0
+
 static cpSpace *space;
 
-typedef struct PlayerStruct {
-	cpFloat u;
-	cpShape *shape;
-} PlayerStruct;
-
-PlayerStruct playerInstance;
+cpBody *playerBody;
+cpShape *playerShape;
 
 static void
 playerUpdateVelocity(cpBody *body, cpVect gravity, cpFloat damping, cpFloat dt)
 {
 	cpBodyUpdateVelocity(body, gravity, damping, dt);
-	body->v.y = cpfmax(body->v.y, -700);
-	body->v.x = cpfclamp(body->v.x, -400, 400);
+//	body->v.y = cpfclamp(body->v.y, -700, -700);
+//	body->v.x = cpfclamp(body->v.x, -400, 400);
 }
 
 static void
 SelectPlayerGroundNormal(cpBody *body, cpArbiter *arb, cpVect *groundNormal){
-	cpVect n = cpArbiterGetNormal(arb, 0);
+	cpVect n = cpvneg(cpArbiterGetNormal(arb, 0));
 	
 	if(n.y > groundNormal->y){
-		*groundNormal = n;
+		(*groundNormal) = n;
 	}
 }
 
 static void
 update(int ticks)
 {
-	static int lastJumpState = 0;
+	static cpBool lastJumpState = cpTrue;
 	int jumpState = (ChipmunkDemoKeyboard.y > 0.0f);
-	
-	cpBody *body = playerInstance.shape->body;
-	
-	cpVect groundNormal = cpvzero;
-	cpBodyEachArbiter(body, (cpBodyArbiterIteratorFunc)SelectPlayerGroundNormal, &groundNormal);
-	
-	if(groundNormal.y > 0.0f){
-		playerInstance.shape->surface_v = cpv(400.0f*ChipmunkDemoKeyboard.x, 0.0f);//cpvmult(cpvperp(groundNormal), 400.0f*arrowDirection.x);
-		if(ChipmunkDemoKeyboard.x) cpBodyActivate(body);
-	} else {
-		playerInstance.shape->surface_v = cpvzero;
-	}
-	
-	// apply jump
-	if(jumpState && !lastJumpState && cpvlengthsq(groundNormal)){
-//		body->v = cpvmult(cpvslerp(groundNormal, cpv(0.0f, 1.0f), 0.5f), 500.0f);
-		body->v = cpvadd(body->v, cpvmult(cpvslerp(groundNormal, cpv(0.0f, 1.0f), 0.75f), 500.0f));
-		cpBodyActivate(body);
-	}
-	
-	if(cpvlengthsq(groundNormal)){
-		cpFloat air_accel = body->v.x + ChipmunkDemoKeyboard.x*(2000.0f);
-		body->f.x = body->m*air_accel;
-//		body->v.x = cpflerpconst(body->v.x, 400.0f*arrowDirection.x, 2000.0f/60.0f);
-	}
 	
 	int steps = 3;
 	cpFloat dt = 1.0f/60.0f/(cpFloat)steps;
 	
 	for(int i=0; i<steps; i++){
+		// Grab the grounding normal from last frame
+		cpVect groundNormal = cpvzero;
+		cpBodyEachArbiter(playerBody, (cpBodyArbiterIteratorFunc)SelectPlayerGroundNormal, &groundNormal);
+		
+		cpBool grounded = (groundNormal.y > 0.0);
+		cpFloat target_vx = PLAYER_VELOCITY*ChipmunkDemoKeyboard.x;
+		
+		// Update the surface velocity and friction
+		cpVect surface_v = cpv(target_vx, 0.0);
+		playerShape->surface_v = surface_v;
+		playerShape->u = (grounded ? PLAYER_GROUND_ACCEL/GRAVITY : 0.0);
+		
+		// Apply air control if not grounded
+		if(!grounded){
+			playerBody->v.x = cpflerpconst(playerBody->v.x, target_vx, PLAYER_AIR_ACCEL*dt);
+		}
+		
+		// If the jump key was just pressed this frame, jump!
+		if(jumpState && !lastJumpState && grounded){
+			playerBody->v = cpvadd(playerBody->v, cpv(0.0, JUMP_VELOCITY));
+			cpBodyActivate(playerBody);
+		}
+		
 		cpSpaceStep(space, dt);
+		lastJumpState = jumpState;
 	}
-	
-	lastJumpState = jumpState;
 }
 
 static cpSpace *
@@ -98,8 +99,9 @@ init(void)
 {
 	space = cpSpaceNew();
 	space->iterations = 10;
-	space->gravity = cpv(0, -1500);
-	space->sleepTimeThreshold = 999999;
+	space->gravity = cpv(0, -GRAVITY);
+//	space->sleepTimeThreshold = 1000;
+	space->enableContactGraph = cpTrue;
 
 	cpBody *body, *staticBody = space->staticBody;
 	cpShape *shape;
@@ -108,57 +110,32 @@ init(void)
 	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(-320,-240), cpv(-320,240), 0.0f));
 	shape->e = 1.0f; shape->u = 1.0f;
 	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
 
 	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(320,-240), cpv(320,240), 0.0f));
 	shape->e = 1.0f; shape->u = 1.0f;
 	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
 
 	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(-320,-240), cpv(320,-240), 0.0f));
 	shape->e = 1.0f; shape->u = 1.0f;
 	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
 	
 	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(-320,240), cpv(320,240), 0.0f));
 	shape->e = 1.0f; shape->u = 1.0f;
 	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
-	
-	// add some other segments to play with
-	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(-220,-200), cpv(-220,240), 0.0f));
-	shape->e = 1.0f; shape->u = 1.0f;
-	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
-
-	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(0,-240), cpv(320,-200), 0.0f));
-	shape->e = 1.0f; shape->u = 1.0f;
-	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
-
-	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(200,-240), cpv(320,-100), 0.0f));
-	shape->e = 1.0f; shape->u = 1.0f;
-	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
-
-	shape = cpSpaceAddShape(space, cpSegmentShapeNew(staticBody, cpv(-220,-80), cpv(200,-80), 0.0f));
-	shape->e = 1.0f; shape->u = 1.0f;
-	shape->layers = NOT_GRABABLE_MASK;
-	shape->collision_type = 2;
 	
 	// Set up the player
-	cpFloat radius = 15.0f;
+	cpFloat radius = 25.0f;
 	body = cpSpaceAddBody(space, cpBodyNew(10.0f, INFINITY));
-	body->p = cpv(0, -220);
+	body->p = cpv(0, -200);
 	body->velocity_func = playerUpdateVelocity;
+	playerBody = body;
 
 	shape = cpSpaceAddShape(space, cpCircleShapeNew(body, radius, cpvzero));
-	shape->e = 0.0f; shape->u = 2.0f;
+	shape->e = 0.0f; shape->u = 0.0f;
 	shape->collision_type = 1;
+	playerShape = shape;
 	
-	playerInstance.u = shape->u;
-	playerInstance.shape = shape;
-	shape->data = &playerInstance;
+//	cpSpaceAddCollisionHandler(space, 1, 0, NULL, PlayerPreStep, NULL, NULL, NULL);
 	
 	return space;
 }
