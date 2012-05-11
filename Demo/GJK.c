9b7f2ba3567e99d7bf52e6406ed8fdd91222333f
slembcke <slembcke@gmail.com>
2012-05-11 12:54:05 -0500

    Playing with GJK

--- Demo/GJK.c
+++ Demo/GJK.c
@@ -30,6 +30,63 @@
 static cpSpace *space;
 static cpShape *shape1, *shape2;
 
+static cpVect
+SupportPoint(cpShape *shape, cpVect n)
+{
+	cpFloat max = -INFINITY;
+	cpVect point = cpvzero;
+	
+	int count = cpPolyShapeGetNumVerts(shape);
+	for(int i=0; i<count; i++){
+		cpVect v = cpBodyLocal2World(shape->body, cpPolyShapeGetVert(shape, i));;
+		cpFloat d = cpvdot(v, n);
+		if(d > max){
+			max = d;
+			point = v;
+		}
+	}
+	
+	return point;
+}
+
+struct SimplexPoint {
+	cpVect a, b, ab;
+};
+
+static struct SimplexPoint
+Support(cpShape *shape1, cpShape *shape2, cpVect n)
+{
+	cpVect a = SupportPoint(shape1, cpvneg(n));
+	cpVect b = SupportPoint(shape2, n);
+	
+	struct SimplexPoint point = {a, b, cpvsub(b, a)};
+	return point;
+}
+
+static cpVect
+Barycentric(cpVect a, cpVect b, cpVect c, cpVect p)
+{
+	cpVect v0 = cpvsub(a, b);
+	cpVect v1 = cpvsub(c, b);
+	cpVect v = cpvsub(p, b);
+
+	cpFloat dot00 = cpvdot(v0, v0);
+	cpFloat dot01 = cpvdot(v0, v1);
+	cpFloat dot0v = cpvdot(v0, v);
+	cpFloat dot11 = cpvdot(v1, v1);
+	cpFloat dot1v = cpvdot(v1, v);
+
+	cpFloat det = dot00*dot11 - dot01*dot01;
+	return cpvmult(cpv(dot11*dot0v - dot01*dot1v, dot00*dot1v - dot01*dot0v), 1.0/det);
+}
+
+static cpFloat
+ClosestT(cpVect a, cpVect b, cpVect p)
+{
+	cpVect delta = cpvsub(b, a);
+	return cpfclamp01(cpvdot(delta, cpvsub(p, a))/cpvlengthsq(delta));
+}
+
 static void
 update(int ticks)
 {
@@ -59,8 +116,8 @@ draw(void)
 	
 	for(int i=0; i<shape1Count; i++){
 		for(int j=0; j<shape2Count; j++){
-			cpVect v1 = cpBodyLocal2World(cpShapeGetBody(shape1), cpPolyShapeGetVert(shape1, i));
-			cpVect v2 = cpBodyLocal2World(cpShapeGetBody(shape2), cpPolyShapeGetVert(shape2, j));
+			cpVect v1 = cpBodyLocal2World(shape1->body, cpPolyShapeGetVert(shape1, i));
+			cpVect v2 = cpBodyLocal2World(shape2->body, cpPolyShapeGetVert(shape2, j));
 			mdiffVerts[i*shape2Count + j] = cpvsub(v2, v1);
 		}
 	}
@@ -70,6 +127,53 @@ draw(void)
 	
 	ChipmunkDebugDrawPolygon(hullCount, hullVerts, RGBAColor(1, 0, 0, 1), RGBAColor(1, 0, 0, 0.25));
 	ChipmunkDebugDrawPoints(2.0, mdiffCount, mdiffVerts, RGBAColor(1, 0, 0, 1));
+	
+	// Draw the axis between the bodies
+//	ChipmunkDebugDrawSegment(shape2->body->p, shape1->body->p, RGBAColor(1, 1, 1, 0.5));
+	
+	// GJK
+	cpVect axis = cpvperp(cpvsub(shape2->body->p, shape1->body->p));
+	struct SimplexPoint v0 = Support(shape1, shape2, axis);
+	struct SimplexPoint v1 = Support(shape1, shape2, cpvneg(axis));
+	
+	for(int i=0; i<10; i++){
+		ChipmunkDebugDrawSegment(v0.ab, v1.ab, RGBAColor(1, 1, 1, 0.5));
+		
+		cpFloat t = ClosestT(v0.ab, v1.ab, cpvzero);
+		cpVect closest = cpvlerp(v0.ab, v1.ab, t);
+		ChipmunkDebugDrawPoints(3.0, 1, &closest, RGBAColor(1, 1, 1, 1));
+		
+		struct SimplexPoint p = Support(shape1, shape2, cpvneg(closest));
+		ChipmunkDebugDrawSegment(closest, p.ab, RGBAColor(0, 1, 0, 1));
+		
+//		cpVect tri[] = {v0.ab, v1.ab, p.ab};
+//		ChipmunkDebugDrawPolygon(3, tri, RGBAColor(1, 1, 1, 0.5), RGBAColor(1, 1, 1, 0.1));
+		
+		cpVect n = cpvnormalize(closest);
+		cpFloat d1 = cpvdot(n, p.ab);
+		cpFloat d2 = d1 - cpfmin(cpvdot(n, v0.ab), cpvdot(n, v1.ab));
+		
+		ChipmunkDemoPrintString("iteration %d, (%5.1f, %5.1f) - (%5.1f, %5.1f) : (%5.1f, %5.1f), %.2e, %.2e", i, v0.ab.x, v0.ab.y, v1.ab.x, v1.ab.y, closest.x, closest.y, d1, d2);
+		ChipmunkDemoPrintString(i%2 == 0 ? "    " : "\n");
+//		cpAssertHard(cpvdot(v0, closest) - cpvdot(v1, closest) == 0.0, "");
+		
+		if(d2 < 0.0){
+			ChipmunkDebugDrawSegment(closest, p.ab, RGBAColor(0, 1, 0, 1));
+			if(cpvlengthsq(v0.ab) < cpvlengthsq(v1.ab)){
+				v1 = p;
+			} else {
+				v0 = p;
+			}
+		} else {
+			ChipmunkDemoPrintString("Not colliding.");
+			
+			cpVect points[] = {cpvlerp(v0.a, v1.a, t), cpvlerp(v0.b, v1.b, t)};
+			ChipmunkDebugDrawPoints(3.0, 2, points, RGBAColor(1, 1, 1, 1));
+			ChipmunkDebugDrawSegment(points[0], points[1], RGBAColor(1, 1, 1, 1));
+			
+			break;
+		}
+	}
 }
 
 static cpSpace *
@@ -77,31 +181,66 @@ init(void)
 {
 	space = cpSpaceNew();
 	cpSpaceSetIterations(space, 5);
+	space->damping = 0.1;
+	
+//	{
+//		cpFloat width = 50.0;
+//		cpFloat height = 70.0;
+//		
+//		cpFloat mass = 1.0f;
+//		cpFloat moment = cpMomentForBox(mass, width, height);
+//		
+//		cpBody *body = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+//		cpBodySetPos(body, cpv(50.0f, 0.0f));
+//		
+//		shape1 = cpSpaceAddShape(space, cpBoxShapeNew(body, width, height));
+//		shape1->group = 1;
+//	}
+//	
+//	{
+//		cpVect verts[] = {{-25, -25}, {-25, 25}, {50, -25}};
+//		
+//		cpFloat mass = 1.0f;
+//		cpFloat moment = cpMomentForPoly(mass, 3, verts, cpvzero);
+//		
+//		cpBody *body = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+//		cpBodySetPos(body, cpv(-50.0f, 0.0f));
+//		
+//		shape2 = cpSpaceAddShape(space, cpPolyShapeNew(body, 3, verts, cpvzero));
+//		shape2->group = 1;
+//	}
 	
 	{
-		cpFloat width = 50.0;
-		cpFloat height = 70.0;
-		
 		cpFloat mass = 1.0f;
-		cpFloat moment = cpMomentForBox(mass, width, height);
+		const int NUM_VERTS = 3;
 		
-		cpBody *body = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+		cpVect verts[NUM_VERTS];
+		for(int i=0; i<NUM_VERTS; i++){
+			cpFloat angle = -2*M_PI*i/((cpFloat) NUM_VERTS);
+			verts[i] = cpv(30*cos(angle), 30*sin(angle));
+		}
+		
+		cpBody *body = cpSpaceAddBody(space, cpBodyNew(mass, cpMomentForPoly(mass, NUM_VERTS, verts, cpvzero)));
 		cpBodySetPos(body, cpv(50.0f, 0.0f));
 		
-		shape1 = cpSpaceAddShape(space, cpBoxShapeNew(body, width, height));
+		shape1 = cpSpaceAddShape(space, cpPolyShapeNew(body, NUM_VERTS, verts, cpvzero));
 		shape1->group = 1;
 	}
 	
 	{
-		cpVect verts[] = {{-25, -25}, {-25, 25}, {50, -25}};
-		
 		cpFloat mass = 1.0f;
-		cpFloat moment = cpMomentForPoly(mass, 3, verts, cpvzero);
+		const int NUM_VERTS = 3;
 		
-		cpBody *body = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+		cpVect verts[NUM_VERTS];
+		for(int i=0; i<NUM_VERTS; i++){
+			cpFloat angle = -2*M_PI*i/((cpFloat) NUM_VERTS);
+			verts[i] = cpv(30*cos(angle), 30*sin(angle));
+		}
+		
+		cpBody *body = cpSpaceAddBody(space, cpBodyNew(mass, cpMomentForPoly(mass, NUM_VERTS, verts, cpvzero)));
 		cpBodySetPos(body, cpv(-50.0f, 0.0f));
 		
-		shape2 = cpSpaceAddShape(space, cpPolyShapeNew(body, 3, verts, cpvzero));
+		shape2 = cpSpaceAddShape(space, cpPolyShapeNew(body, NUM_VERTS, verts, cpvzero));
 		shape2->group = 1;
 	}
 	
