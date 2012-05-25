6514a09dd8165ebd53e0a62e857519304fefdad1
slembcke <slembcke@gmail.com>
2012-05-25 08:18:14 -0500

    bumping version number

--- include/chipmunk/chipmunk.h
+++ include/chipmunk/chipmunk.h
@@ -106,10 +106,10 @@ typedef struct cpSpace cpSpace;
 
 #include "cpSpace.h"
 
-// Chipmunk 6.1.0
+// Chipmunk 6.1.1
 #define CP_VERSION_MAJOR 6
 #define CP_VERSION_MINOR 1
-#define CP_VERSION_RELEASE 0
+#define CP_VERSION_RELEASE 1
 
 /// Version string.
 extern const char *cpVersionString;
