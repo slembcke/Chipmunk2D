0042022154d37d51cb3c72e32541fc0b8841c953
slembcke <slembcke@gmail.com>
2012-06-15 12:11:30 -0500

    doc link fix and some private API changes

--- src/cpSpaceStep.c
+++ src/cpSpaceStep.c
@@ -23,19 +23,13 @@
 
 //MARK: Post Step Callback Functions
 
-typedef struct cpPostStepCallback {
-	cpPostStepFunc func;
-	void *key;
-	void *data;
-} cpPostStepCallback;
-
-void *
-cpSpaceGetPostStepData(cpSpace *space, void *key)
+cpPostStepCallback *
+cpSpaceGetPostStepCallback(cpSpace *space, void *key)
 {
 	cpArray *arr = space->postStepCallbacks;
 	for(int i=0; i<arr->num; i++){
 		cpPostStepCallback *callback = (cpPostStepCallback *)arr->arr[i];
-		if(callback->key == key) return callback->data;
+		if(callback->key == key) return callback;
 	}
 	
 	return NULL;
@@ -50,17 +44,17 @@ cpSpaceAddPostStepCallback(cpSpace *space, cpPostStepFunc func, void *key, void 
 		"Adding a post-step callback when the space is not locked is unnecessary. "
 		"Post-step callbacks will not called until the end of the next call to cpSpaceStep() or the next query.");
 	
-	void *prev = cpSpaceGetPostStepData(space, key);
-	if(!prev){
+	if(!cpSpaceGetPostStepCallback(space, key)){
 		cpPostStepCallback *callback = (cpPostStepCallback *)cpcalloc(1, sizeof(cpPostStepCallback));
 		callback->func = (func ? func : PostStepDoNothing);
 		callback->key = key;
 		callback->data = data;
 		
 		cpArrayPush(space->postStepCallbacks, callback);
+		return TRUE;
+	} else {
+		return FALSE;
 	}
-	
-	return (prev == NULL);
 }
 
 //MARK: Locking Functions
