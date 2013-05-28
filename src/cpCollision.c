a5dc712e31255acc10e67b5076d051be39fa2657
slembcke <slembcke@gmail.com>
2013-05-28 11:53:04 -0500

    derp

--- src/cpCollision.c
+++ src/cpCollision.c
@@ -679,7 +679,6 @@ static const CollisionFunc builtinCollisionFuncs[9] = {
 	(CollisionFunc)seg2poly,
 	(CollisionFunc)poly2poly,
 };
-static const CollisionFunc *colfuncs = segmentCollisions;
 
 static const CollisionFunc segmentCollisions[9] = {
 	(CollisionFunc)circle2circle,
@@ -692,6 +691,8 @@ static const CollisionFunc segmentCollisions[9] = {
 	(CollisionFunc)seg2poly,
 	(CollisionFunc)poly2poly,
 };
+
+static const CollisionFunc *colfuncs = segmentCollisions;
 
 void
 cpEnableSegmentToSegmentCollisions(void)
