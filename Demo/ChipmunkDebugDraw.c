b5c065f1ae8e87c34cf0467ca608669cdded7647
Leonid S. Usov <leonid.s.usov@gmail.com>
2013-09-14 17:36:36 +0300

    [msvc] fix segmentation fault due to usage of APPLE glew api

--- Demo/ChipmunkDebugDraw.c
+++ Demo/ChipmunkDebugDraw.c
@@ -131,8 +131,13 @@ ChipmunkDebugDrawInit(void)
 	CHECK_GL_ERRORS();
 	
 	// Setu VBO and VAO.
+#if __APPLE__
 	glGenVertexArraysAPPLE(1, &vao);
 	glBindVertexArrayAPPLE(vao);
+#else
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+#endif
 	
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -143,8 +148,12 @@ ChipmunkDebugDrawInit(void)
 	SET_ATTRIBUTE(program, struct Vertex, outline_color, GL_FLOAT);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+#if __APPLE__
 	glBindVertexArrayAPPLE(0);
-	
+#else
+	glBindVertexArray(0);
+#endif
+
 	CHECK_GL_ERRORS();
 }
 
@@ -549,7 +558,11 @@ ChipmunkDebugDrawFlushRenderer(void)
 	glUseProgram(program);
 	glUniform1f(glGetUniformLocation(program, "u_outline_coef"), ChipmunkDebugDrawPointLineScale);
 	
+#if __APPLE__
 	glBindVertexArrayAPPLE(vao);
+#else
+	glBindVertexArray(vao);
+#endif
 	glDrawArrays(GL_TRIANGLES, 0, triangle_count*3);
 		
 	CHECK_GL_ERRORS();
