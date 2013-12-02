e0a66e60e8a1eab106f9288175f1c1c5735d08e0
slembcke <slembcke@gmail.com>
2013-12-02 10:54:24 -0600

    Updating license comments.

--- src/prime.h
+++ src/prime.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2007 Scott Lembcke
+/* Copyright (c) 2013 Scott Lembcke and Howling Moon Software
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +18,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
+
 // Used for resizing hash tables.
 // Values approximately double.
 // http://planetmath.org/encyclopedia/GoodHashTablePrimes.html
